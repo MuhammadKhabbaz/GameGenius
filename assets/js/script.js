14a2692b3671647ec12c9c03b103c542750ba4b0
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-10-02 00:13:27 +1100

    Added pagination function and fixed search functions, added some filter functions tentative

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,109 @@
 var searchButtonEl = $('#buttonSearch');
 var gamelistEl = $('#gameLists');
+var gameCards = $('#gameCards');
+
+var prevBtnEl = $('#prevBtn');
+var nextBtnEl = $('#nextBtn');
+
+var prev;
+var next;
 
 // RAWG API key
-const apiKey = '3cdf9cc32da5448dbb6bfe6c7afa0561';
+const apiKey = '?key=3cdf9cc32da5448dbb6bfe6c7afa0561';
+const rawgUrl = 'https://api.rawg.io/api/games';
 
 // RAWG API function call for search
-// args: query for search input
-function rawgGames(query) {
-    var url = 'https://api.rawg.io/api/games?key=' + apiKey + '&search=' + query;
+// args: endpoint as input
+function rawgGames(url) {
+    // Hide news section of page
+    console.log(url);
+    $('#newsSection').css('display', 'none');
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.results);
-            var searchResults = data.results;
-            for (var i = 0; i < searchResults.length; i++){
-                console.log(searchResults[i].name);
-                generateGameList(searchResults[i]);
-            }
+            console.log(data);
+            // Call function to generate game lists based on data results
+            generateGameList(data.results);
+            // Display prev and next buttons
+            prevBtnEl.css('display', 'inline');
+            nextBtnEl.css('display', 'inline');
+            prev = data.previous
+            next = data.next
+            localStorage.setItem('prev', JSON.stringify(prev));
+            localStorage.setItem('next', JSON.stringify(next));
+            // Check if prev and next is null or not
+            if (prev === null){
+                prevBtnEl.css('display', 'none');
+            } else if (next === null){
+                nextBtnEl.css('display', 'none');
+            } 
         })
 }
 
-function generateGameList(results){
-    var gameListItemEl = $('<li class="">');
-    var gameImgEL = $('<img>').attr('src', results.background_image);
-    gameImgEL.css({'width' : '10%' , 'height' : '10%'});
-    var gameNameEl = $('<h3>').text(results.name);
-    gameListItemEl.append(gameNameEl, gameImgEL);
-    gamelistEl.append(gameListItemEl);
+// When search button is clicked add paremeters to endpoint and call api
+function handleSearchButton(){
+    gamelistEl.empty();
+    console.log('SEARCH PRESSED');
+    var searchInputEl = '&search=';
+    searchInputEl += $('#searchInput').val().trim();
+    searchInputEl += filterGenres();
+    rawgGames(rawgUrl+apiKey+searchInputEl);
 }
 
+// Search button event
+searchButtonEl.on('click', handleSearchButton);
+
+// TODO: Need add a function for filter parameters and add it to search or call another api
+function filterGenres(){
+    var genreParam = '&genres=';
+    // Get from genres tags inputs and append to genreParam var
+    genreParam += 'racing'
+    return genreParam;
+}
+
+
+function generateGameList(searchResults){
+    for (var i = 0; i < searchResults.length; i++){
+        // console.log(searchResults[i].name);
+
+        // TODO need to add class name for css game cards
+        var gameListItemEl = $('<li class="">');
+        var gameImgEL = $('<img>').attr('src', searchResults[i].background_image);
+        // sample might be removed
+        gameImgEL.css({'width' : '10%' , 'height' : '10%'});
+        var gameNameEl = $('<h3>').text(searchResults[i].name);
+        gameListItemEl.append(gameNameEl, gameImgEL);
+        gamelistEl.append(gameListItemEl);
+    }
+}
+
+
+// Initial Pagination functions
+// When next button is clicked move next results
+nextBtnEl.on('click', function () {
+    console.log('NEXT BUTTON_--------------')
+    console.log(next);
+    gamelistEl.empty();
+    rawgGames(next);
+});
+
+// When prev button is clicked move prev results
+prevBtnEl.on('click', function () {
+    console.log('PREV BUTTON_--------------')
+    console.log(prev);
+    gamelistEl.empty();
+    rawgGames(prev);
+});
+
+
+
+
+// SOME OTHER CATEGORIES WE MIGHT NEED
 // List of genres available for RAWG API
 function getGenres(){
-    var url = 'https://api.rawg.io/api/genres?key=' + apiKey;
+    var url = 'https://api.rawg.io/api/genres' + apiKey;
     fetch(url)
         .then(function (response) {
             return response.json();
@@ -43,11 +113,11 @@ function getGenres(){
             console.log(data);
         })
 }
-// getGenres();
+getGenres();
 
 // List of Platforms available for RAWG API
 function getPlatforms(){
-    var url = 'https://api.rawg.io/api/platforms?key=' + apiKey;
+    var url = 'https://api.rawg.io/api/platforms' + apiKey;
     fetch(url)
         .then(function (response) {
             return response.json();
@@ -61,14 +131,13 @@ function getPlatforms(){
 // getPlatforms();
 
 
-
-function handleFormSubmit(){
-    var searchInputEl = $('#searchInput').val();
-    rawgGames(searchInputEl.trim());
-    $('#newsSection').css('display', 'none');
-    //populate stuff
+// Initialize page
+function init(){
+    // Hide pagination buttons on landing page
+    prevBtnEl.css('display', 'none');
+    nextBtnEl.css('display', 'none');
+    localStorage.setItem('prev', JSON.stringify(null));
+    localStorage.setItem('next', JSON.stringify(null));
 
 }
-
-// Search button event
-searchButtonEl.on('click', handleFormSubmit);
+init();
