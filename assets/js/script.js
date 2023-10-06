101a8dada2aea451a135c8c19285f0b7fc7b3b43
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-10-06 22:17:27 +1100

    Added game info for game news

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,9 +32,7 @@ function rawgGames(url) {
             // Call function to generate game lists based on data resulst
             generateGameList(data.results);
             // Display prev and next buttons
-
             buttonSortBy.css('display', 'block');
-
             prevBtnEl.css('display', 'inline');
             nextBtnEl.css('display', 'inline');
             prev = data.previous
@@ -50,6 +48,136 @@ function rawgGames(url) {
         })
 }
 
+//*************************** FILTER FUNCTIONS ***************************
+
+function search(){
+    var searchParam = '&search=';
+    var searchmade = $('#searchInput');
+    searchParam += searchmade.val().trim();
+    return searchParam;
+}
+function filterGenres(){
+    var genreParam = '&genres=';
+    // Get from genres tags inputs and append to genreParam var
+    var genreselected = $("#genreSelector");
+    genreParam += genreselected.val()
+    return genreParam;
+}
+function filterPlatforms(){
+    var platformParam = '&parent_platforms=';
+    var platformSelected = $("#platformSelector")
+    platformParam += platformSelected.val()
+    return platformParam;
+}
+function filterNoPlayers(){
+    var playersParam = '&tags='
+    var playersSelected = $("#playersSelector")
+    playersParam += playersSelected.val()
+    return playersParam;
+}
+
+//*************************** GENERATE GAME CARD ***************************
+// Generate game card results
+function generateGameList(searchResults) {
+    for (var i = 0; i < searchResults.length; i++) {
+        console.log(searchResults[i].name);
+        // Create list for game results
+        var gameListItemEl = $('<li class="box">');
+        var gameImgEL = $('<img class="imgCard">').attr('src', searchResults[i].background_image);
+        // Create link element for clickable game title
+        var gameNameEl = $('<a>').text(searchResults[i].name).attr('data-gameid', searchResults[i].id);
+        gameNameEl.addClass('gameLink');
+        gameNameEl.css('display', 'block');
+        // Call getRating to get game rating
+        var gameRatingEl = getRating(searchResults[i].rating_top);
+        // Call getPlatformList to create available platforms in icons
+        var gamePlatformEl = getPlatformList(searchResults[i]);
+        var gameGenres = getGenreList(searchResults[i].genres);
+        var gameESRB = $('<h5 class="subtitle is-6">');
+        if (searchResults[i].esrb_rating !== null) {
+            gameESRB.text(searchResults[i].esrb_rating.name)
+        } else {
+            gameESRB.text('None');
+        }
+        // Append everything to list element
+        gameListItemEl.append(gameNameEl, gameImgEL, gameRatingEl, gamePlatformEl, gameGenres, gameESRB);
+        gamelistEl.append(gameListItemEl);
+    }
+}
+
+// Generate platforms
+function getPlatformList(results) {
+    var iconRef = ['pc', 'playstation', 'xbox', 'ios', 'nintendo'];
+    var iconArr = ['windows', 'playstation', 'xbox', 'apple', 'nintendo'];
+    var iconClass = ['fa-brands fa-windows', 'fa-brands fa-playstation', 'fa-brands fa-xbox', 'fa-brands fa-apple', 'fa-solid fa-gamepad'];
+    
+    var platformArr = [];
+    var platforms = results.parent_platforms
+    var divPlatform = $('<div class="level-left">');
+    
+    for (var i = 0; i < platforms.length; i++) {
+        platformArr.push(platforms[i].platform.slug);
+    }
+    console.log(platformArr);
+    
+    for (var i = 0; i < platformArr.length; i++) {
+        if (iconRef.includes(platformArr[i])) {
+            var iconIndex = iconRef.indexOf(platformArr[i]);
+            var icons = $('<i>').addClass(iconClass[iconIndex]);
+            var span = $('<span class="icon is-small">');
+            span.append(icons);
+            var link = $('<a id="' + iconArr[iconIndex] + '" class="level-item"  aria-label="reply">')
+            link.append(span);
+            divPlatform.append(link);
+        }
+    }
+    return divPlatform;
+}
+
+// Get ratings of games
+function getRating(num) {
+    console.log(num);
+    var divRating = $('<p>')
+    
+    for (var i = 1; i <= 5; i++) {
+        var rating = $('<i>');
+        if (num >= i) {
+            // create full star
+            rating.addClass('fa-solid fa-star');
+        } else {
+            // create empty star
+            rating.addClass('fa-regular fa-star');
+        }
+        
+        divRating.append(rating);
+    }
+    divRating.append(' Rating');
+    return divRating;
+}
+
+// Get genre list for the game
+function getGenreList(genres){
+    var div = $('<div class="tags">');
+    for (var i = 0; i < genres.length; i++) {
+        var genre = $('<span class="tag">').text(genres[i].name);
+        div.append(genre);
+    }
+    return div;
+}
+
+
+//*************************** CLICK EVENT FUNCTIONS ***************************
+
+function handleGameLink(event) {
+    // var gameLinkClicked = (event.target);
+    var element = event.target;
+    if (element.matches("a") === true) {
+        var gameID = element.dataset.gameid
+        console.log(gameID);
+        localStorage.setItem('gameID', JSON.stringify(gameID));
+        window.location.replace('gamePage.html');
+    }
+}
 
 // When search button is clicked add paremeters to endpoint and call api
 function handleSearchButton() {
@@ -73,9 +201,22 @@ function handleSearchButton() {
 
 }
 
+
+//*************************** CLICK EVENTS ***************************
+
+// Selected game link event
+gamelistEl.on('click', '.gameLink', handleGameLink);
 // Search button event
 searchButtonEl.on('click', handleSearchButton);
-// Pressing enter key
+
+$('#gameNews1').on('click', function(){
+    var gameID = "845261";
+    localStorage.setItem('gameID', JSON.stringify(gameID));
+    window.location.replace('gamePage.html');
+});
+// Game title returns to landing page
+$('#gameGeniusTitle').on('click', function () { window.location.reload();})
+// Pressing enter key on search input
 $('#searchInput').keypress(function (e) {
     var key = e.which;
     if (key == 13)  // the enter key code
@@ -85,161 +226,17 @@ $('#searchInput').keypress(function (e) {
     }
 });
 
-
-// TODO: Need add a function for filter parameters and add it to search or call another api
-
-function search(){
-    var searchParam = '&search=';
-    var searchmade = $('#searchInput');
-    searchParam += searchmade.val().trim();
-    return searchParam;
-}
-function filterGenres(){
-    var genreParam = '&genres=';
-    // Get from genres tags inputs and append to genreParam var
-    var genreselected = $("#genreSelector");
-    genreParam += genreselected.val()
-
-    return genreParam;
-}
-function filterPlatforms(){
-    var platformParam = '&parent_platforms=';
-    var platformSelected = $("#platformSelector")
-    platformParam += platformSelected.val()
-    return platformParam;
-}
-function filterNoPlayers(){
-    var playersParam = '&tags='
-    var playersSelected = $("#playersSelector")
-    playersParam += playersSelected.val()
-    return playersParam;
-}
-
-
-
-function generateGameList(searchResults) {
-    for (var i = 0; i < searchResults.length; i++) {
-        console.log(searchResults[i].name);
-
-        // Create list for game results
-        var gameListItemEl = $('<li class="box">');
-
-        var gameImgEL = $('<img class="imgCard">').attr('src', searchResults[i].background_image);
-        // Create link element for clickable game title
-        var gameNameEl = $('<a>').text(searchResults[i].name).attr('data-gameid', searchResults[i].id);
-
-        gameNameEl.addClass('gameLink');
-        gameNameEl.css('display', 'block');
-        // Call getRating to get game rating
-        var gameRatingEl = getRating(searchResults[i].rating_top);
-        // Call getPlatformList to create available platforms in icons
-        var gamePlatformEl = getPlatformList(searchResults[i]);
-
-        var gameGenres = getGenreList(searchResults[i].genres);
-        var gameESRB = $('<h5 class="subtitle is-6">');
-        if (searchResults[i].esrb_rating !== null) {
-            gameESRB.text(searchResults[i].esrb_rating.name)
-        } else {
-            gameESRB.text('None');
-        }
-        // Append everything to list element
-        gameListItemEl.append(gameNameEl, gameImgEL, gameRatingEl, gamePlatformEl, gameGenres, gameESRB);
-
-        gamelistEl.append(gameListItemEl);
-    }
-}
-
-function handleGameLink(event) {
-    // var gameLinkClicked = (event.target);
-    var element = event.target;
-    if (element.matches("a") === true) {
-        var gameID = element.dataset.gameid
-        console.log(gameID);
-        localStorage.setItem('gameID', JSON.stringify(gameID));
-        window.location.replace('gamePage.html');
-    }
-
-}
-
-gamelistEl.on('click', '.gameLink', handleGameLink);
-
-// Generate platforms
-function getPlatformList(results) {
-    var iconRef = ['pc', 'playstation', 'xbox', 'ios', 'nintendo'];
-    var iconArr = ['windows', 'playstation', 'xbox', 'apple', 'nintendo'];
-    var iconClass = ['fa-brands fa-windows', 'fa-brands fa-playstation', 'fa-brands fa-xbox', 'fa-brands fa-apple', 'fa-solid fa-gamepad'];
-
-    var platformArr = [];
-    var platforms = results.parent_platforms
-    var divPlatform = $('<div class="level-left">');
-
-    for (var i = 0; i < platforms.length; i++) {
-        platformArr.push(platforms[i].platform.slug);
-    }
-    console.log(platformArr);
-
-    for (var i = 0; i < platformArr.length; i++) {
-        if (iconRef.includes(platformArr[i])) {
-            var iconIndex = iconRef.indexOf(platformArr[i]);
-            var icons = $('<i>').addClass(iconClass[iconIndex]);
-            var span = $('<span class="icon is-small">');
-            span.append(icons);
-            var link = $('<a id="' + iconArr[iconIndex] + '" class="level-item"  aria-label="reply">')
-            link.append(span);
-            divPlatform.append(link);
-        }
-    }
-    return divPlatform;
-}
-
-// Generate ratings
-function getRating(num) {
-    console.log(num);
-    var divRating = $('<p>')
-
-    for (var i = 1; i <= 5; i++) {
-        var rating = $('<i>');
-        if (num >= i) {
-            // create full star
-            rating.addClass('fa-solid fa-star');
-        } else {
-            // create empty star
-            rating.addClass('fa-regular fa-star');
-        }
-
-        divRating.append(rating);
-    }
-    divRating.append(' Rating');
-    return divRating;
-}
-
-function getGenreList(genres){
-    var div = $('<div class="tags">');
-    for (var i = 0; i < genres.length; i++) {
-        var genre = $('<span class="tag">').text(genres[i].name);
-        div.append(genre);
-    }
-    return div;
-}
-
-
-// Initial Pagination functions
-// When next button is clicked move next results
+// Pagination Next button
 nextBtnEl.on('click', function () {
-    console.log('NEXT BUTTON_--------------')
-    console.log(next);
     gamelistEl.empty();
     rawgGames(next);
 });
 
-// When prev button is clicked move prev results
+// Pagination Previous button
 prevBtnEl.on('click', function () {
-    console.log('PREV BUTTON_--------------')
-    console.log(prev);
     gamelistEl.empty();
     rawgGames(prev);
 });
-
 
 //*************************** SOME OTHER CATEGORIES WE MIGHT NEED ***************************
 
@@ -288,24 +285,6 @@ function getPlatforms(){
     return platformList;
         }
 
-// function getMaturityRating(){
-//     var maturityList = []
-//     var url = 'https://api.rawg.io/api/games/{id}' + apiKey;
-//     fetch(url)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         // Do something/ call a function
-//         .then(function (data) {
-//             console.log(data);
-//             for (var i =0; i<data.results.length; i++) {
-//                 maturityList.push(data.results[i].slug);
-//             }
-//             console.log("ml="+maturityList);
-//         })
-//     return maturityList;
-// }
-
 function getNOofPlayers(){
     var NoPlayers = []
     var url = 'https://api.rawg.io/api/tags' + apiKey;
@@ -324,10 +303,28 @@ function getNOofPlayers(){
         })
     return NoPlayers;
 }
-// Returns to landing page
-$('#gameGeniusTitle').on('click', function () {
-    window.location.reload();
-})
+
+
+// function getLatestGames() {
+//     // var genreList = [];
+//     var url = 'https://api.rawg.io/api/games' + apiKey + '&ordering=-metacritic,-released';
+//     fetch(url)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         // Do something/ call a function
+//         .then(function (data) {
+//             console.log(data);
+//             // for (var i = 0; i < data.results.length; i++) {
+//             //     genreList.push(data.results[i].slug);
+//             // }
+//             // console.log(genreList);
+//         })
+//     // return genreList;
+// }
+
+
+
 
 // Initialize page
 function init() {
@@ -339,7 +336,7 @@ function init() {
     localStorage.setItem('prev', JSON.stringify(null));
     localStorage.setItem('next', JSON.stringify(null));
     localStorage.setItem('gameID', JSON.stringify(null));
-    // console.log('GENRE--------');
+    
     // getGenres();
     // getPlatforms();
     // getNOofPlayers();
